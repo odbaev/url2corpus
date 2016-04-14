3d8066257359e1f265137f455d41cc62e8772924
odbaev <odbaev@yandex.ru>
2016-04-14 19:34:01 +0300

    added getting html code from url

--- UrlCorpus.cpp
+++ UrlCorpus.cpp
@@ -1,7 +1,42 @@
 #include "UrlCorpus.h"
 
-void UrlCorpus::getCorpus(const char *url, const char *path)
+UrlCorpus::UrlCorpus()
+{
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+}
+
+UrlCorpus::~UrlCorpus()
+{
+    curl_easy_cleanup(curl);
+}
+
+
+void UrlCorpus::getCorpus(const char* url, const char* path)
 {
 
 }
+
+size_t UrlCorpus::writeCallback(char* contents, size_t size, size_t nmemb, string* userp)
+{
+    userp->append(contents, size * nmemb);
+    return size * nmemb;
+}
+
+string UrlCorpus::getHtml(const char* url)
+{
+    string html;
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &html);
+    curl_easy_perform(curl);
+
+    return html;
+}
+
+
+
+
+
 
--- UrlCorpus.h
+++ UrlCorpus.h
@@ -1,11 +1,23 @@
 #ifndef URL2CORPUS_URLCORPUS_H
 #define URL2CORPUS_URLCORPUS_H
 
+#include <iostream>
+#include <curl/curl.h>
+
+using namespace std;
 
 class UrlCorpus
 {
 public:
+    UrlCorpus();
+    ~UrlCorpus();
     void getCorpus(const char* url, const char* path);
+
+private:
+    static size_t writeCallback(char* contents, size_t size, size_t nmemb, string* userp);
+    string getHtml(const char* url);
+
+    CURL* curl;
 };
 
 
